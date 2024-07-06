bdbd4fb6e0d5699908dcdc92a31f49a8ee9e1773
Thejas <teju270934@gmail.com>
2024-07-07 00:33:26 +0530

    styled the sub page of the website

--- stuydysyncs-sub1/src/index.js
+++ stuydysyncs-sub1/src/index.js
@@ -31,17 +31,73 @@ function Header() {
           className="login-icon icon-download"
         />
         <img src="images/user-2.jpg" alt="Login" className="login-image" />
+        <p className="nav--name">Zoya</p>
       </ul>
     </nav>
   );
 }
 
 function Main() {
-  return <main>Main content here</main>;
+  return (
+    <>
+      <div className="main--content">
+        <div className="side-bar">
+          {" "}
+          <ul className="sidebar-list">
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+          </ul>
+        </div>
+        <div className="background">
+          <div className="bg--content">
+            <div>
+              <img
+                src="./images/background.jpeg"
+                alt="background_img"
+                className="bg--img"
+              ></img>
+              <p className="bg--text">
+                Jss Science and Technology University, Mysuru
+              </p>
+            </div>
+
+            <div>
+              <div className="boxes">
+                <div className="box-1">
+                  <ul className="course--list">
+                    <p className="course--name">Courses Offered</p>
+                    <li className="course--item">1.Computer Science</li>
+                    <li className="course--item">2.Information Science</li>
+                    <li className="course--item">
+                      3.Electronics and Communication{" "}
+                    </li>
+                    <li className="course--item">
+                      4.Electrical and Instrumental
+                    </li>
+                    <li className="course--item">5.Civil Engineering</li>
+                  </ul>
+                </div>
+                <div className="box-2">
+                  <p className="content-2--text">
+                    "At our college, we foster innovation, academic excellence,
+                    and community engagement, preparing students for successful,
+                    impactful futures."
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 function Footer() {
-  return <footer>Footer content here</footer>;
+  return <footer></footer>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
